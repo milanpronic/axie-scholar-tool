ae424ec5413d07b5be792aa227143c5444d69827
Sevenstarsandokan <sandokan72176@gmail.com>
2021-09-01 13:29:46 +0900

    update src/index.js React.StrictMode => React.Fragment

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import 'mdbreact/dist/css/mdb.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <BrowserRouter>
       <App />
     </BrowserRouter>
 
 
-  </React.StrictMode>,
+  </React.Fragment>,
   document.getElementById('root')
 );
 
